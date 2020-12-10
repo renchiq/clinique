68f915110e56b223434908b51c65437c65f7baa8
Rinat Galyamshin <risky00@mail.ru>
2020-12-10 17:37:32 +0300

    changes in Post class

--- stomatology/home/models.py
+++ stomatology/home/models.py
@@ -5,7 +5,12 @@ from django.utils.timezone import now
 class Post(models.Model):
     title = models.CharField(verbose_name='Загловок', max_length=200)
     pub_date = models.DateField(verbose_name='Дата публикации', default=now())
-    post_type_image = models.SmallIntegerField(verbose_name='Тип поста')
+    post_types = (
+        (1, 'Новости'),
+        (2, 'Акции'),
+        (3, 'Развлекательная информация')
+    )
+    post_type_image = models.SmallIntegerField(verbose_name='Тип поста', choices=post_types)
     description = models.CharField(verbose_name='Описание поста', max_length=500)
     content = models.TextField(verbose_name='Содержание поста')
 
